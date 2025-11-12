819bebceddd29a9e9d7974a915b04ca0599f4ab4
matejbabiak <9matej.babiak@gmail.com>
2025-11-12 20:15:58 +0100

    add models

--- main.py
+++ main.py
@@ -1,27 +1,24 @@
 from fastapi import FastAPI, HTTPException
 from enum import Enum
-
+from schemas import GenreURLChoices, Band 
 
 app = FastAPI()
 
-class GenreURLChoices(Enum):
-    ROCK = 'rock'
-    ELECTRONIC = 'electronic'
-    HIP_HOP = 'hip-hop'
-
 BANDS = [
-    {'id':1, 'name':'The Kinks', 'genre':'Rock'},
+    {'id':1, 'name':'The Kinks', 'genre':'Rock', 'albums': [{'title': 'master of reality', 'release_date': '1971-07-21'}]},
     {'id':2, 'name':'Aphex Twin', 'genre':'Electronic'},
     {'id':3, 'name':'Wu-Tang Clan', 'genre':'Hip-Hop'},
 ]
 
 @app.get("/bands")
-async def bands()-> list[dict]:
-    return BANDS
+async def bands()-> list[Band]:
+    return [
+        Band(**b) for b in BANDS
+    ]
 
 @app.get("/bands/{band_id}")
-async def get_band(band_id: int) -> dict:
-    band = next((b for b in BANDS if b['id'] == band_id), None)
+async def get_band(band_id: int) -> Band:
+    band = next((Band(**b) for b in BANDS if b['id'] == band_id), None)
     if band is None:
         raise HTTPException(status_code=404, detail = 'Band not found')
     return band
---
+++ schemas.py
@@ -0,0 +1,21 @@
+
+from enum import Enum
+from pydantic import BaseModel
+from datetime import date
+
+class GenreURLChoices(Enum):
+    ROCK = 'rock'
+    ELECTRONIC = 'electronic'
+    HIP_HOP = 'hip-hop'
+
+class Album(BaseModel):
+    title: str
+    release_date: date
+
+class Band(BaseModel):
+    id:int
+    name:str
+    genre:str
+    albums: list[Album] = []
+
+
